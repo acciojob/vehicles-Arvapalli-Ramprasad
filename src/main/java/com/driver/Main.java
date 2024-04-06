95971b4a04c69fd553bf176c62cd46506d87a039
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-06 13:28:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,5 +6,10 @@ public class Main {
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+
+
+        Boat boat = new Boat("Boaty McBoatface", 10);
+        System.out.println("Boat name: " + boat.getVehicleName());
+        System.out.println("Boat capacity: " + boat.getVehicleCapacity());
     }
 }
