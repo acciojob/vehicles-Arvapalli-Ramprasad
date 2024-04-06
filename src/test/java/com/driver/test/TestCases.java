1546355fcb1c30c91e9a5d29169c727accf560f5
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-06 13:35:11 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,5 +1,30 @@
 package com.driver.test;
 
-public class TestCases {
+import com.driver.Boat;
+import com.driver.F1;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class TestCases {
+  public void testF1Acceleration() {
+        F1 f1 = new F1("Ferrari", false);
+        assertEquals(0, f1.getCurrentSpeed());
+
+        f1.move(50, 0);
+        assertEquals(50, f1.getCurrentSpeed());
+
+        f1.accelerate(30);
+        assertEquals(80, f1.getCurrentSpeed());
+
+        f1.accelerate(-40);
+        assertEquals(40, f1.getCurrentSpeed());
+
+        f1.accelerate(-60); // Should stop the car
+        assertEquals(0, f1.getCurrentSpeed());
+    }
+    public void testBoat() {
+        Boat boat = new Boat("Titanic", 3000);
+        assertEquals("Titanic", boat.getVehicleName());
+        assertEquals(3000, boat.getVehicleCapacity());
+    }
 }
