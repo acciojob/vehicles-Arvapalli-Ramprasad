c1984c05a01466f7ed12f98273802b864cfea841
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-06 16:26:39 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,16 +2,30 @@ package com.driver.test;
 
 import com.driver.Boat;
 import com.driver.F1;
-import org.junit.Test;
+import com.driver.Car;
+import com.driver.Vehicle;
+import com.driver.WaterVehicle;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestCases {
+  
     @Test
-    public void testF1Acceleration() {
-        F1 f1 = new F1("Ferrari", false);
-        assertEquals(0, f1.getCurrentSpeed());
+    public void testCar1() {
+        Car car = new Car("Toyota", 4, 4, 5, true, "Sedan", 5);
 
+        assertEquals(4, car.getWheels());
+        assertEquals(4, car.getDoors());
+        assertEquals(5, car.getGears());
+        assertTrue(car.isManual());
+        assertEquals("Sedan", car.getType());
+        assertEquals(5, car.getSeats());
+    }
+
+    @Test
+    public void testF11() {
+        F1 f1 = new F1("Ferrari", false);
         f1.move(50, 0);
         assertEquals(50, f1.getCurrentSpeed());
 
@@ -23,12 +37,5 @@ public class TestCases {
 
         f1.accelerate(-60); // Should stop the car
         assertEquals(0, f1.getCurrentSpeed());
-    }
-
-    @Test
-    public void testBoat() {
-        Boat boat = new Boat("Titanic", 3000);
-        assertEquals("Titanic", boat.getVehicleName());
-        assertEquals(3000, boat.getVehicleCapacity());
     }
 }
